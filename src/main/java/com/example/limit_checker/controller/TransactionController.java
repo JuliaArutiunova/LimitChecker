22c6d6c0ab0118630290f90e395528b490301839
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-04-11 13:17:38 +0300

    Add Swagger documentation

--- src/main/java/com/example/limit_checker/controller/TransactionController.java
+++ src/main/java/com/example/limit_checker/controller/TransactionController.java
@@ -1,13 +1,24 @@
 package com.example.limit_checker.controller;
 
 import com.example.limit_checker.dto.TransactionCreateDto;
+import com.example.limit_checker.dto.error.ErrorResponseDto;
 import com.example.limit_checker.service.api.ITransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transaction")
+@Tag(name = "Transaction API",
+        description = "API for receiving and processing transactions from banking services")
 public class TransactionController {
     private final ITransactionService transactionService;
 
@@ -17,6 +28,26 @@ public class TransactionController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+            summary = "Receive Transaction",
+            description = "Endpoint for receiving and processing new transactions from banking services.",
+            requestBody = @RequestBody(
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = TransactionCreateDto.class))
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "201",
+                            description = "Transaction successfully received and processed."),
+                    @ApiResponse(responseCode = "400",
+                            description = "Invalid transaction data provided.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class))),
+                    @ApiResponse(responseCode = "500",
+                            description = "Internal Server Error",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
     public void save(@RequestBody @Valid TransactionCreateDto transactionCreateDto) {
         transactionService.create(transactionCreateDto);
     }
--- src/main/java/com/example/limit_checker/controller/UserController.java
+++ src/main/java/com/example/limit_checker/controller/UserController.java
@@ -3,8 +3,18 @@ package com.example.limit_checker.controller;
 import com.example.limit_checker.dto.LimitCreateDto;
 import com.example.limit_checker.dto.LimitInfoDto;
 import com.example.limit_checker.dto.TransactionInfoDto;
+import com.example.limit_checker.dto.error.ErrorResponseDto;
 import com.example.limit_checker.service.api.ILimitService;
 import com.example.limit_checker.service.api.ITransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +23,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "Client API",
+        description = "Client-facing API for managing user account limits and retrieving transaction information.")
 public class UserController {
 
     private final ITransactionService transactionService;
@@ -24,6 +36,27 @@ public class UserController {
     }
 
     @GetMapping("/{account}/limits")
+    @Operation(
+            summary = "Get Limits",
+            description =
+                    "Retrieves the list of limits for a specific user account. Optionally filters by expense category.",
+            parameters = {
+                    @Parameter(name = "account",
+                            in = ParameterIn.PATH, description = "Account identifier.",
+                            required = true, schema = @Schema(type = "string", example = "0001234567")),
+                    @Parameter(name = "expenseCategory ",
+                            in = ParameterIn.QUERY, description = "Optional filter by expense category.",
+                            required = false, schema = @Schema(type = "string", example = "product"))
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved account limits.",
+                            content = @Content(array = @ArraySchema(
+                                    schema = @Schema(implementation = LimitInfoDto.class)))),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
     public List<LimitInfoDto> getLimits(@PathVariable("account") String account,
                                         @RequestParam(value = "expenseCategory ", required = false)
                                         String expenseCategory) {
@@ -36,12 +69,56 @@ public class UserController {
 
     @PostMapping("/{account}/limits")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+            summary = "Set New Limit",
+            description = "Sets a new limit for a specific user account and expense category.",
+            parameters = {
+                    @Parameter(name = "account", in = ParameterIn.PATH, description = "Account identifier.",
+                            required = true, schema = @Schema(type = "string", example = "0001234567"))
+            },
+            requestBody = @RequestBody(
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = LimitCreateDto.class))
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Limit successfully created."),
+                    @ApiResponse(responseCode = "400", description = "Invalid limit data provided.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
     public void createLimit(@PathVariable("account") String account,
                             @RequestBody @Valid LimitCreateDto limitCreateDto) {
         limitService.create(account, limitCreateDto);
     }
 
     @GetMapping("/{account}/transactions/exceeded-limit")
+    @Operation(
+            summary = "Get Transactions Exceeding Limit",
+            description =
+                    "Retrieves a list of transactions for a specific user account that exceeded their spending limit. " +
+                            "Optionally filters by expense category.",
+            parameters = {
+                    @Parameter(name = "account",
+                            in = ParameterIn.PATH, description = "Account identifier.",
+                            required = true, schema = @Schema(type = "string", example = "0001234567")),
+                    @Parameter(name = "expenseCategory ",
+                            in = ParameterIn.QUERY, description = "Optional filter by expense category.",
+                            required = false, schema = @Schema(type = "string", example = "Shopping"))
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Successfully retrieved transactions exceeding the limit.",
+                            content = @Content(array = @ArraySchema(schema
+                                    = @Schema(implementation = TransactionInfoDto.class)))),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
     public List<TransactionInfoDto> get(@PathVariable("account") String account,
                                         @RequestParam(value = "expenseCategory ", required = false)
                                         String expenseCategory) {
