242b1baaa43c709ec64c0c4e6914836b79726918
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-04-11 03:22:50 +0300

    Implement transaction saving functionality

--- src/main/java/com/example/limit_checker/controller/TransactionController.java
+++ src/main/java/com/example/limit_checker/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.example.limit_checker.controller;
 
 import com.example.limit_checker.dto.TransactionCreateDto;
+import com.example.limit_checker.service.api.ITransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
+    private final ITransactionService transactionService;
+
+    public TransactionController(ITransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody @Valid TransactionCreateDto transactionCreateDto) {
+        transactionService.create(transactionCreateDto);
     }
 }
--- src/main/java/com/example/limit_checker/service/TransactionService.java
+++ src/main/java/com/example/limit_checker/service/TransactionService.java
@@ -3,12 +3,18 @@ package com.example.limit_checker.service;
 import com.example.limit_checker.dto.TransactionCreateDto;
 import com.example.limit_checker.dto.TransactionInfoDto;
 import com.example.limit_checker.repository.api.ITransactionRepository;
+import com.example.limit_checker.repository.entity.LimitEntity;
+import com.example.limit_checker.repository.entity.TransactionEntity;
 import com.example.limit_checker.repository.projection.TransactionInfoProjection;
+import com.example.limit_checker.service.api.IExchangeRateService;
+import com.example.limit_checker.service.api.ILimitService;
 import com.example.limit_checker.service.api.ITransactionService;
 
 import com.example.limit_checker.service.mapper.TransactionInfoMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -16,33 +22,78 @@ public class TransactionService implements ITransactionService {
 
     private final ITransactionRepository transactionRepository;
     private final TransactionInfoMapper transactionInfoMapper;
+    private final ILimitService limitService;
+    private final IExchangeRateService exchangeRateService;
 
 
     public TransactionService(ITransactionRepository transactionRepository,
-                              TransactionInfoMapper transactionInfoMapper) {
+                              TransactionInfoMapper transactionInfoMapper, ILimitService limitService, IExchangeRateService exchangeRateService) {
         this.transactionRepository = transactionRepository;
         this.transactionInfoMapper = transactionInfoMapper;
+        this.limitService = limitService;
+        this.exchangeRateService = exchangeRateService;
     }
 
     @Override
+    @Transactional
     public void create(TransactionCreateDto transactionCreateDto) {
+        String accountFrom = transactionCreateDto.getAccountFrom();
+        String expenseCategory = transactionCreateDto.getExpenseCategory();
+        String transactionCurrency = transactionCreateDto.getCurrencyShortname();
+
+        LimitEntity limitEntity = limitService.defineLimit(accountFrom, expenseCategory);
+
+        BigDecimal convertedSum;
+        if (!transactionCreateDto.getCurrencyShortname().equals(limitService.getDefaultCurrency())) {
+            convertedSum = convertSum(transactionCreateDto.getSum(),
+                    transactionCurrency, limitEntity.getCurrencyShortname());
+        } else {
+            convertedSum = transactionCreateDto.getSum();
+        }
+
+        TransactionEntity transactionEntity = TransactionEntity.builder()
+                .accountFrom(accountFrom)
+                .accountTo(transactionCreateDto.getAccountTo())
+                .limit(limitEntity)
+                .sum(transactionCreateDto.getSum())
+                .currencyShortname(transactionCurrency)
+                .expenseCategory(expenseCategory)
+                .datetime(transactionCreateDto.getDatetime())
+                .build();
+
+        BigDecimal sumSpent = limitEntity.getCurrentSpent().add(convertedSum);
+
+        if (limitEntity.getSum().compareTo(sumSpent) < 0) {
+            transactionEntity.setLimitExceeded(true);
+        }
+        limitEntity.setCurrentSpent(sumSpent);
+
+        limitService.save(limitEntity);
+        transactionRepository.saveAndFlush(transactionEntity);
 
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<TransactionInfoDto> getAllExceededLimit(String account) {
 
         List<TransactionInfoProjection> projections = transactionRepository
                 .findExceededLimitByAccountOrderByDateDesc(account);
 
         return transactionInfoMapper.toTransactionInfoDtoList(projections);
-
     }
+
     @Override
-    public List<TransactionInfoDto> getAllExceededLimitByExpenseCategory(String account, String expenseCategory){
+    @Transactional(readOnly = true)
+    public List<TransactionInfoDto> getAllExceededLimitByExpenseCategory(String account, String expenseCategory) {
         List<TransactionInfoProjection> projections = transactionRepository
                 .findExceededLimitByAccountAndCategoryOrderByDateDesc(account, expenseCategory);
         return transactionInfoMapper.toTransactionInfoDtoList(projections);
+    }
+
+    private BigDecimal convertSum(BigDecimal sum, String currencyBase, String currencyQuote) {
+        BigDecimal rate = exchangeRateService.getCurrentRate(currencyBase, currencyQuote);
+        return sum.multiply(rate);
     }
 
 
